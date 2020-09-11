e31229658cdc47888f7274be2e8be5370e44810f
Max <jaimies.dev@gmail.com>
2020-09-11 11:55:28 +0300

    create TweetRepositoryImpl

---
+++ src/main/ts/data/TweetRepositoryImpl.ts
@@ -0,0 +1,11 @@
+import {TweetRepository} from "../domain/repository/TweetRepository"
+import {Tweet} from "../domain/model/Tweet"
+import {User} from "../domain/model/User"
+
+export class TweetRepositoryImpl implements TweetRepository {
+    getTweets(): Tweet[] {
+        const fakeUser = new User("Fake User", "fakeuser")
+        const fakeTweet = new Tweet("Fake tweet", fakeUser)
+        return [fakeTweet]
+    }
+}
