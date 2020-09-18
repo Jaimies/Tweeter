7b591a92e9db3396d14d6cabac4a6e0e4844db60
Max <jaimies.dev@gmail.com>
2020-09-18 22:02:31 +0300

    Enhance the fake sign up functionality to allow logging in to a newly created account

--- src/main/ts/data/AuthRepositoryImpl.ts
+++ src/main/ts/data/AuthRepositoryImpl.ts
@@ -1,26 +1,29 @@
 import {AuthRepository} from "../domain/repository/AuthRepository"
 import {User} from "../domain/model/User"
 
-const fakeUser = new User("fakeuser", "Fake User")
 const users = [{id: "fakeuser", password: "fakeuser"}]
 
 function credentialsAreValid(credential: string, password: string) {
     return users.some(user => user.id == credential && user.password == password)
 }
 
+function createUserWithId(id: string | null): User | null {
+    if (id == null) return null
+    return new User(id, id)
+}
+
 export class AuthRepositoryImpl implements AuthRepository {
     user: User | null
 
     constructor() {
-        const isSignedIn = localStorage.getItem("isSignedIn") == "true"
-        this.user = isSignedIn ? fakeUser : null
+        this.user = createUserWithId(localStorage.getItem("userId"))
     }
 
     login(credential: string, password: string) {
         if (!credentialsAreValid(credential, password))
             return false
 
-        localStorage.setItem("isSignedIn", "true")
+        localStorage.setItem("userId", credential)
         return true
     }
 
