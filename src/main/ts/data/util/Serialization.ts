245f008d52cd005d3906fb31b7b2ae91c7f1a6a4
Max <jaimies.dev@gmail.com>
2020-09-21 10:20:23 +0300

    Remove the unneeded deserializeUserEntry method

--- src/main/ts/data/util/Serialization.ts
+++ src/main/ts/data/util/Serialization.ts
@@ -1,6 +1,5 @@
 import {Tweet} from "../../domain/model/Tweet"
 import {User} from "../../domain/model/User"
-import {UserEntry} from "../UserEntry"
 
 export function deserializeTweet(object: any) {
     return new Tweet(object.body, deserializeUser(object.author), new Date(object.date))
@@ -8,8 +7,4 @@ export function deserializeTweet(object: any) {
 
 export function deserializeUser(object: any) {
     return new User(object.id, object.name, object.email)
-}
-
-export function deserializeUserEntry(object: any) {
-    return new UserEntry(deserializeUser(object.user), object.password)
 }
