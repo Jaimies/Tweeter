1f0fdbf16d95940a7020d081e027728baf096acd
Max <jaimies.dev@gmail.com>
2020-09-22 16:58:31 +0300

    Cover GetCurrentUserUseCase with tests

---
+++ src/test/domain/usecase/StubUserRepository.ts
@@ -0,0 +1,10 @@
+import {UserRepository} from "../../../main/domain/repository/UserRepository"
+import {User} from "../../../main/domain/model/User"
+
+export class StubUserRepository implements UserRepository {
+    constructor(private users: User[]) {}
+
+    addUser = () => {}
+    findUserById = (userId: string) => this.users.find(user => user.id == userId)
+    getUsers = () => this.users
+}
