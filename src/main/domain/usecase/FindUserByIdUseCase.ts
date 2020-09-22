49246d3b5bcafc19eb2eeadf96ba274fe6c31985
Max <jaimies.dev@gmail.com>
2020-09-22 12:06:42 +0300

    Add the functionality for displaying user profiles

---
+++ src/main/domain/usecase/FindUserByIdUseCase.ts
@@ -0,0 +1,10 @@
+import {UserRepository} from "../repository/UserRepository"
+import {User} from "../model/User"
+
+export class FindUserByIdUseCase {
+    constructor(private userRepository: UserRepository) {}
+
+    run(id: string): User | undefined {
+        return this.userRepository.findUserById(id)
+    }
+}
