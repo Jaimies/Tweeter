f8b079b563c7584cbeae08953c63479bbe2a4048
Max <jaimies.dev@gmail.com>
2020-09-22 12:12:55 +0300

    Make the user list items links

--- src/main/ui/views/UserListItem.vue
+++ src/main/ui/views/UserListItem.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="listItem">
+   <RouterLink :to="`/` + user.id" class="listItem routerLink">
     <div class="listItem__header">{{ user.name }}</div>
     <div class="listItem__secondaryHeader">@{{ user.id }}</div>
     <p>User's profile description.</p>
-  </div>
+  </RouterLink>
 </template>
 
 <script>
