8f99bae0784643965f0cf824458c27315cfa0c44
Max <jaimies.dev@gmail.com>
2020-09-22 09:48:12 +0300

    Add the functionality for displaying list of user profiles

---
+++ src/main/ui/views/UserListItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="listItem">
+    <div class="listItem__header">{{ user.name }}</div>
+    <div class="listItem__secondaryHeader">@{{ user.id }}</div>
+    <p>User's profile description.</p>
+  </div>
+</template>
+
+<script>
+import {User} from "../../domain/model/User"
+
+export default {
+  props: {
+    user: User
+  }
+}
+</script>
