14e673210e3979cade862cadfc84a8340112ee47
Max <jaimies.dev@gmail.com>
2020-09-24 11:40:46 +0300

    Add the functionality for selecting tweets only from a specific user in feed

--- src/main/shared/RxOperators.ts
+++ src/main/shared/RxOperators.ts
@@ -4,3 +4,7 @@ import {map} from "rxjs/operators"
 export function mapList<T, R>(mapFunction: (input: T) => R): OperatorFunction<T[], R[]> {
     return map(list => list.map(mapFunction))
 }
+
+export function filterList<T>(mapFunction: (input: T) => boolean): OperatorFunction<T[], T[]> {
+    return map(list => list.filter(mapFunction))
+}
