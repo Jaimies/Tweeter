8103987ed8187c30ae01718142b82902e7c7f0db
Max <jaimies.dev@gmail.com>
2020-09-27 12:57:06 +0300

    Add updateUser method to StubUserRepository

--- src/test/domain/usecase/StubUserRepository.ts
+++ src/test/domain/usecase/StubUserRepository.ts
@@ -5,6 +5,7 @@ export class StubUserRepository implements UserRepository {
     constructor(private users: User[]) {}
 
     addUser = () => {}
+    updateUser = () => {}
     findUserById = (userId: string) => this.users.find(user => user.id == userId)
     getUsers = () => this.users
 }
