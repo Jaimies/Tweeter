66d8b4949895deef9edcb2ecb42400fae0da57b4
Max <jaimies.dev@gmail.com>
2020-09-29 08:53:07 +0300

    Use UiUser instead of User in UserCard.vue

--- src/main/ui/model/UiUser.ts
+++ src/main/ui/model/UiUser.ts
@@ -1,9 +1,13 @@
 import {User} from "../../domain/model/User"
 
 export class UiUser {
-    constructor(public name: string, public id: string) {}
+    constructor(
+        public name: string,
+        public id: string,
+        public bio: string
+    ) {}
 }
 
-export function mapUserToPresentation({id, name}: User) {
-    return new UiUser(name, id)
+export function mapUserToPresentation({id, name, bio}: User) {
+    return new UiUser(name, id, bio)
 }
