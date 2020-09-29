e9b7b384b15fe00c19df575224f0028d10df0c3a
Max <jaimies.dev@gmail.com>
2020-09-29 23:12:48 +0300

    Show follow button in user profile

---
+++ src/main/ui/views/ToggleButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button @click="$emit('toggle')">
+    <span v-if="enabled"><slot name="enabledText"/></span>
+    <span v-else><slot name="disabledText"/></span>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    enabled: Boolean
+  }
+}
+</script>
