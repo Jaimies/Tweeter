1568f2666a51602f2dad97f4cb7ddf5304d2de1f
Max <jaimies.dev@gmail.com>
2020-09-30 20:58:22 +0300

    fix: Add missing parameter in SignUp.vue

--- src/main/ui/components/SignUp.vue
+++ src/main/ui/components/SignUp.vue
@@ -43,7 +43,7 @@ export default {
     },
 
     createUser() {
-      const user = new User(this.username, this.name, this.email, "")
+      const user = new User(this.username, this.name, this.email, "", [])
       provideSignUpUseCase().run(user, this.password)
     },
 
