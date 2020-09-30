fc59191177af2a865b9f1af3e3c70771130db31a
Max <jaimies.dev@gmail.com>
2020-09-30 12:12:10 +0300

    Show tweets by followed users in feed

--- src/main/domain/usecase/GetTweetsUseCase.ts
+++ src/main/domain/usecase/GetTweetsUseCase.ts
@@ -1,9 +1,14 @@
 import {TweetRepository} from "../repository/TweetRepository"
+import {GetCurrentUserUseCase} from "./GetCurrentUserUseCase"
+import {Tweet} from "../model/Tweet"
+import {Observable} from "rxjs"
 
 export class GetTweetsUseCase {
-    constructor(private tweetRepository: TweetRepository) {}
+    constructor(private tweetRepository: TweetRepository, private getUser: GetCurrentUserUseCase) {}
 
-    run(userId: string) {
-        return this.tweetRepository.getTweetsByUserId(userId)
+    run(userId: string) : Observable<Tweet[]>{
+        const user = this.getUser.run()
+        const ids = user?.id == userId ? [userId, ...user.following] : [userId]
+        return this.tweetRepository.getTweetsByUserIds(ids)
     }
 }
