59a475f28ede3b4559c8f733afa0ae9b65b09423
Max <jaimies.dev@gmail.com>
2020-10-02 11:17:51 +0300

    Cover ToggleButton with tests

---
+++ src/test/ui/views/ToggleButton.test.ts
@@ -0,0 +1,31 @@
+/** @jest-environment jsdom */
+
+import ToggleButton from "../../../main/ui/views/ToggleButton"
+import {mount, Wrapper} from "@vue/test-utils"
+import {Vue} from "vue/types/vue"
+
+function mountComponent(enabled: boolean): Wrapper<Vue> {
+    return mount(ToggleButton, {
+        propsData: {enabled},
+        slots: {
+            enabledText: "Enabled",
+            disabledText: "Disabled"
+        }
+    })
+}
+
+it("show enabled text if enabled", () => {
+    const wrapper = mountComponent(true)
+    expect(wrapper.text()).toEqual("Enabled")
+})
+
+it("shows disabled text if disabled", () => {
+    const wrapper = mountComponent(false)
+    expect(wrapper.text()).toEqual("Disabled")
+})
+
+it("emits toggle event when clicked", () => {
+    const wrapper = mountComponent(true)
+    wrapper.trigger("click")
+    expect(wrapper.emitted()).toHaveProperty("toggle")
+})
