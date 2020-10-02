abd5eff308cb8d20dd146fd01b99d67e71e5dc32
Max <jaimies.dev@gmail.com>
2020-10-02 11:57:53 +0300

    Cover BaseModal with tests

---
+++ src/test/ui/views/BaseModal.test.ts
@@ -0,0 +1,12 @@
+/** @jest-environment jsdom */
+
+import {mount} from "@vue/test-utils"
+import BaseModal from "../../../main/ui/views/BaseModal"
+
+it("emits close event only if overlay is clicked", () => {
+    const wrapper = mount(BaseModal)
+    wrapper.find(".content").trigger("click")
+    expect(wrapper.emitted()).not.toHaveProperty("close")
+    wrapper.trigger("click")
+    expect(wrapper.emitted()).toHaveProperty("close")
+})
