4ea79d853a9d7e046a1d354b017c4f3b1c3cc637
Max <jaimies.dev@gmail.com>
2020-10-02 22:23:58 +0300

    Put getTimeLabel.ts's dependencies into its file

--- src/main/ui/util/getFullDaysSince.ts
+++
@@ -1,6 +0,0 @@
-const MILLIS_IN_DAY = 1000 * 3600 * 24
-
-export function getFullDaysSince(date: Date): number {
-    const millisSince = new Date().getTime() - date.getTime()
-    return Math.floor(millisSince / MILLIS_IN_DAY)
-}
