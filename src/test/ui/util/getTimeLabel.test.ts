81ad988f1b301800d7ac4fb736b2d9c14cb479e1
Max <jaimies.dev@gmail.com>
2020-10-02 13:25:41 +0300

    Join getTimeLabel.ts and its dependencies into a single file, make tests more meaningful

--- src/test/ui/util/getTimeLabel.test.ts
+++ src/test/ui/util/getTimeLabel.test.ts
@@ -1,20 +1,18 @@
 import MockDate from "mockdate"
 import {getTimeLabel} from "../../../main/ui/util/getTimeLabel"
-import {getTimeSince} from "../../../main/ui/util/getTimeSince"
-import {toReadableDate} from "../../../main/ui/util/toReadableDate"
 
-jest.mock("../../../main/ui/util/getTimeSince", () => ({getTimeSince: jest.fn()}))
-jest.mock("../../../main/ui/util/toReadableDate", () => ({toReadableDate: jest.fn()}))
-
-beforeAll(() => MockDate.set("2020-01-01Z"))
+beforeAll(() => MockDate.set("2020-01-01T00:00:00Z"))
 afterAll(() => MockDate.reset())
 
-it("calls getTimeSince", () => {
-    getTimeLabel(new Date("2019-12-01Z"))
-    expect(toReadableDate).toBeCalled()
-})
-
-it("calls toReadableDate", () => {
-    getTimeLabel(new Date("2019-12-31T22:00:00Z"))
-    expect(getTimeSince).toBeCalled()
+it("getTimeLabel()", () => {
+    expect(getTimeLabel(new Date("2019-11-01Z"))).toBe("Nov 1, 2019")
+    expect(getTimeLabel(new Date("2019-12-01Z"))).toBe("Dec 1, 2019")
+    expect(getTimeLabel(new Date("2019-12-02Z"))).toBe("Dec 2, 2019")
+    expect(getTimeLabel(new Date("2019-12-31T12:00:00Z"))).toBe("12h")
+    expect(getTimeLabel(new Date("2019-12-31T23:00:00Z"))).toBe("1h")
+    expect(getTimeLabel(new Date("2019-12-31T23:01:00Z"))).toBe("59m")
+    expect(getTimeLabel(new Date("2019-12-31T23:59:00Z"))).toBe("1m")
+    expect(getTimeLabel(new Date("2019-12-31T23:59:01Z"))).toBe("59s")
+    expect(getTimeLabel(new Date("2019-12-31T23:59:59Z"))).toBe("1s")
+    expect(getTimeLabel(new Date("2020-01-01T00:00:00Z"))).toBe("1s")
 })
