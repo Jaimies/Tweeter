edf5d4d85cde14d2c7ba47c3c225d008d0624d68
Max <jaimies.dev@gmail.com>
2020-10-03 22:12:43 +0300

    Refactor UserRepositoryImpl.test.ts

--- src/test/data/UserRepositoryImpl.test.ts
+++ src/test/data/UserRepositoryImpl.test.ts
@@ -1,53 +1,50 @@
 import {UserRepositoryImpl} from "../../main/data/UserRepositoryImpl"
 import {FakeStorage} from "./FakeStorage"
 import {UserRepository} from "../../main/domain/repository/UserRepository"
-import {User} from "../../main/domain/model/User"
-import {testUser} from "../testData"
+import {createTestUser} from "../testData"
+import {Storage} from "../../main/data/Storage"
+
+const [user, anotherUser] = [createTestUser(), createTestUser()]
 
 let userRepository: UserRepository
+let storage: Storage
 
-function initializeUserRepository(users: User[]) {
-    const storage = new FakeStorage({users})
+beforeEach(() => {
+    storage = new FakeStorage({users: [user]})
     userRepository = new UserRepositoryImpl(storage)
-}
-
-it("initializes with no users", () => {
-    initializeUserRepository([])
-    expect(userRepository.getUsers()).toEqual([])
 })
 
-it("initializes with some users", () => {
-    initializeUserRepository([testUser])
-    expect(userRepository.getUsers()).toEqual([testUser])
+it("addUser()", () => {
+    userRepository.addUser(anotherUser)
+    expect(userRepository.getUsers()).toEqual([user, anotherUser])
 })
 
-it("adds a user", () => {
-    initializeUserRepository([])
-    userRepository.addUser(testUser)
-    expect(userRepository.getUsers()).toEqual([testUser])
+describe("findUserById()", () => {
+    it("returns the needed user", () => {
+        expect(userRepository.findUserById(user.id)).toEqual(user)
+    })
+
+    it("returns undefined if there is no user with such id", () => {
+        expect(userRepository.findUserById("12341234")).toBe(undefined)
+    })
 })
 
-it("gets user by id", () => {
-    initializeUserRepository([testUser])
-    expect(userRepository.findUserById(testUser.id)).toEqual(testUser)
-})
-
-it("returns undefined when trying to get a user by non-existent id", () => {
-    initializeUserRepository([testUser])
-    expect(userRepository.findUserById("12341234")).toBe(undefined)
-})
-
-it("updates user", () => {
-    initializeUserRepository([testUser])
-    const updatedUser = {...testUser, name: "New Name"}
-    userRepository.updateUser(updatedUser)
-    expect(userRepository.findUserById(testUser.id)).toEqual(updatedUser)
-})
-
-it("doesn't allow to change user id", () => {
-    initializeUserRepository([testUser])
-    const updatedUser = {...testUser, id: "newuserid"}
-    expect(() => {
+describe("updateUser()", () => {
+    it("allows to change name", () => {
+        const updatedUser = {...user, name: "New Name"}
         userRepository.updateUser(updatedUser)
-    }).toThrow()
+        expect(userRepository.findUserById(user.id)).toEqual(updatedUser)
+    })
+
+    it("doesn't allow to change id", () => {
+        const updatedUser = {...user, id: "newuserid"}
+        expect(() => {
+            userRepository.updateUser(updatedUser)
+        }).toThrow()
+    })
+})
+
+it("persists data", () => {
+    const newUserRepository = new UserRepositoryImpl(storage)
+    expect(newUserRepository.getUsers()).toEqual([user])
 })
