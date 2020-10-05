9bd3ac52f87784e7377648c0d8fc003e3291a7c3
Max <jaimies.dev@gmail.com>
2020-10-05 10:02:19 +0300

    Improve test names

--- src/test/shared/RxOperators.test.ts
+++ src/test/shared/RxOperators.test.ts
@@ -2,13 +2,13 @@ import {of} from "rxjs"
 import {filterList, mapList} from "../../main/shared/RxOperators"
 import {expectObservableValue} from "../RxTestUtils"
 
-it("mapList", done => {
+it("mapList()", done => {
     const observable = of([1, 2])
     const mappedObservable = observable.pipe(mapList(item => item.toString()))
     expectObservableValue(mappedObservable, ["1", "2"], done)
 })
 
-it("filterList", done => {
+it("filterList()", done => {
     const observable = of([1, 2])
     const mappedObservable = observable.pipe(filterList(item => item == 2))
     expectObservableValue(mappedObservable, [2], done)
