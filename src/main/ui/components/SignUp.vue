45907fb4bd54f894a498835205604d286659c420
Max <jaimies.dev@gmail.com>
2020-10-06 20:24:08 +0300

    Show validation errors in sign up page

--- src/main/ui/components/SignUp.vue
+++ src/main/ui/components/SignUp.vue
@@ -1,11 +1,21 @@
 <template>
-  <BaseForm :isValid="!$v.$invalid" buttonText="Sign up"
-            headingText="Sign up to twitter" @submit="signUp">
+  <BaseForm :isValid="!$v.$invalid"
+            buttonText="Sign up"
+            headingText="Sign up to twitter"
+            @submit="signUp">
+
     <BaseInput v-model="email" label="Email" type="email"/>
+    <p class="error" v-if="!$v.email.isUnique">Email has already been taken.</p>
+    <p class="error" v-else-if="!$v.email.email">Please enter a valid email.</p>
+
     <BaseInput v-model="username" label="Username"/>
+    <p class="error" v-if="!$v.username.isUnique">Username has already been taken.</p>
+
     <BaseInput v-model="name" label="Name"/>
+
     <BaseInput v-model="password" label="Password" type="password"/>
     <BaseInput v-model="confirmPassword" label="Confirm password" type="password"/>
+    <p class="error" v-if="!$v.confirmPassword.sameAsPassword">Passwords do not match.</p>
   </BaseForm>
 </template>
 
@@ -14,7 +24,9 @@ import BaseInput from "../views/BaseInput"
 import BaseForm from "../views/BaseForm"
 import {User} from "../../domain/model/User"
 import {email, minLength, required, sameAs} from "vuelidate/lib/validators"
-import {provideSignUpUseCase} from "../../di/provideUseCases"
+import {provideCheckCredentialAvailabilityUseCase, provideSignUpUseCase} from "../../di/provideUseCases"
+
+const checkAvailability = provideCheckCredentialAvailabilityUseCase()
 
 export default {
   components: {BaseForm, BaseInput},
@@ -27,8 +39,15 @@ export default {
   }),
 
   validations: {
-    email: {email, required},
-    username: {required},
+    email: {
+      email,
+      required,
+      isUnique: value => checkAvailability.isEmailAvailable(value)
+    },
+    username: {
+      required,
+      isUnique: value => checkAvailability.isUserIdAvailable(value)
+    },
     password: {required, minLength: minLength(8)},
     confirmPassword: {
       sameAsPassword: sameAs("password")
