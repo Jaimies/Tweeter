98fbf5a89f30fbc89e2ec82cb0fb08be170a33c2
Max <jaimies.dev@gmail.com>
2020-10-09 15:57:24 +0300

    Fix invalid StubAuthRepository implementation

--- src/test/domain/usecase/StubAuthRepository.ts
+++ src/test/domain/usecase/StubAuthRepository.ts
@@ -2,6 +2,7 @@ import {AuthRepository} from "../../../main/domain/repository/AuthRepository"
 
 export class StubAuthRepository implements AuthRepository {
     constructor(public userId: string | undefined) {}
-    login = () => true
+    login = () => {}
+    areCredentialsValid = () => true
     signUp = () => {}
 }
