569fd77e0bb242819cf341174641edf7140efbb3
Max <jaimies.dev@gmail.com>
2020-10-09 17:04:56 +0300

    refactor: clean up CheckIfLoggedInUseCase.test.ts

--- src/test/domain/usecase/CheckIfLoggedInUseCase.test.ts
+++ src/test/domain/usecase/CheckIfLoggedInUseCase.test.ts
@@ -1,18 +1,20 @@
 import {CheckIfLoggedInUseCase} from "@/domain/usecase/CheckIfLoggedInUseCase"
 import {StubAuthRepository} from "./StubAuthRepository"
+import {AuthRepository} from "@/domain/repository/AuthRepository"
 
 let useCase: CheckIfLoggedInUseCase
+let authRepository: AuthRepository
 
-function initializeUseCase(userId: string | undefined) {
-    useCase = new CheckIfLoggedInUseCase(new StubAuthRepository(userId))
-}
+beforeEach(() => {
+    authRepository = new StubAuthRepository(undefined)
+    useCase = new CheckIfLoggedInUseCase(authRepository)
+})
 
 it("return false when user is not signed in", () => {
-    initializeUseCase(undefined)
     expect(useCase.run()).toBe(false)
 })
 
 it("returns true when user is signed in", () => {
-    initializeUseCase("abc")
+    authRepository.userId = "abc"
     expect(useCase.run()).toBe(true)
 })
