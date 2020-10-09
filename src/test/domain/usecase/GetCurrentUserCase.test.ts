c675cbf9d5abc0ed5b688f63c2c574435e343334
Max <jaimies.dev@gmail.com>
2020-10-09 17:07:15 +0300

    refactor: clean up GetCurrentUserCase.test.ts

--- src/test/domain/usecase/GetCurrentUserCase.test.ts
+++ src/test/domain/usecase/GetCurrentUserCase.test.ts
@@ -1,22 +1,23 @@
 import {GetCurrentUserUseCase} from "@/domain/usecase/GetCurrentUserUseCase"
+import {AuthRepository} from "@/domain/repository/AuthRepository"
 import {StubAuthRepository} from "./StubAuthRepository"
 import {StubUserRepository} from "./StubUserRepository"
 import {testUser} from "../../testData"
 
 let useCase: GetCurrentUserUseCase
+let authRepository: AuthRepository
 
-function initializeUseCase(userId: string | undefined) {
-    const authRepository = new StubAuthRepository(userId)
+beforeEach(() => {
+    authRepository = new StubAuthRepository(undefined)
     const userRepository = new StubUserRepository([testUser])
     useCase = new GetCurrentUserUseCase(authRepository, userRepository)
-}
+})
 
 it("returns undefined if there is no signed in user", () => {
-    initializeUseCase(undefined)
     expect(useCase.run()).toBeUndefined()
 })
 
 it("returns the current user if one exists", () => {
-    initializeUseCase(testUser.id)
+    authRepository.userId = testUser.id
     expect(useCase.run()).toBe(testUser)
 })
