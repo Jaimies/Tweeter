bde67e3ff1d5970ee7a80df3b0734327a492eead
Max <jaimies.dev@gmail.com>
2020-10-17 15:26:11 +0300

    feat: follow button in user list

--- src/main/ui/components/user/ListItem.vue
+++ src/main/ui/components/user/ListItem.vue
@@ -1,15 +1,19 @@
 <template>
-   <RouterLink :to="`/` + user.id" class="card routerLink block">
+  <RouterLink :to="`/` + user.id" class="card routerLink block">
     <div class="header">{{ user.name }}</div>
     <div class="secondaryHeader">@{{ user.id }}</div>
     <p class="bio">{{ user.bio }}</p>
+
+    <FollowButton :user="user" @click.native.prevent/>
   </RouterLink>
 </template>
 
 <script>
 import {User} from "@/domain/model/User"
+import FollowButton from "@/ui/components/user/FollowButton"
 
 export default {
+  components: {FollowButton},
   props: {
     user: User
   }
