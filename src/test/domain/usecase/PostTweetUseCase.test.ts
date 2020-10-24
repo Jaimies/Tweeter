47056f3e391451cd856eecc7f2645b4808b6f9fa
Max <jaimies.dev@gmail.com>
2020-10-24 19:23:23 +0300

    test: cover PostTweetUseCase

---
+++ src/test/domain/usecase/PostTweetUseCase.test.ts
@@ -0,0 +1,22 @@
+import {PostTweetUseCase} from "@/domain/usecase/PostTweetUseCase"
+import {MockTweetRepository} from "./MockTweetRepository"
+import {StubGetCurrentUserUseCase} from "./StubGetCurrentUserUseCase"
+import {testUser} from "../../testData"
+import {GetCurrentUserUseCase} from "@/domain/usecase/GetCurrentUserUseCase"
+import {Tweet} from "@/domain/model/Tweet"
+import MockDate from "mockdate"
+
+const tweetRepository = new MockTweetRepository()
+const getCurrentUserUseCase = new StubGetCurrentUserUseCase(testUser)
+const useCase = new PostTweetUseCase(tweetRepository, getCurrentUserUseCase as unknown as GetCurrentUserUseCase)
+
+const currentDate = new Date()
+
+beforeAll(() => MockDate.set(currentDate))
+
+it("calls addTweet() with the right arguments", () => {
+    useCase.run("Tweet body")
+    expect(tweetRepository.addTweet).toBeCalledWith(
+        new Tweet("Tweet body", testUser, currentDate)
+    )
+})
