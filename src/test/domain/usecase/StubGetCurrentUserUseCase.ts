f1c8e6ab333d2f7198bdcd74649449850ae4e200
Max <jaimies.dev@gmail.com>
2020-10-24 19:21:12 +0300

    refactor(StubGetCurrentUserUseCase): allow for undefined user value

--- src/test/domain/usecase/StubGetCurrentUserUseCase.ts
+++ src/test/domain/usecase/StubGetCurrentUserUseCase.ts
@@ -1,7 +1,7 @@
 import {User} from "@/domain/model/User"
 
 export class StubGetCurrentUserUseCase {
-    constructor(private user: User) {}
+    constructor(private user: User | undefined) {}
 
     run() {
         return this.user
