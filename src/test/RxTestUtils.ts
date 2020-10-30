c0057914d35d50e704854d3e494cff109291ab13
Max <jaimies.dev@gmail.com>
2020-10-30 19:57:29 +0200

    refactor(RxTestUtils): remove unused expectObservableValue() function

--- src/test/RxTestUtils.ts
+++ src/test/RxTestUtils.ts
@@ -1,10 +1,6 @@
 import {Observable} from "rxjs"
 import {first} from "rxjs/operators"
 
-export async function expectObservableValue<T>(observable: Observable<T>, expectedValue: T) {
-    expect(await getValue(observable)).toEqual(expectedValue)
-}
-
 export function getValue<T>(observable: Observable<T>): Promise<T> {
     return new Promise(resolve => {
         observable.pipe(first()).subscribe(resolve)
