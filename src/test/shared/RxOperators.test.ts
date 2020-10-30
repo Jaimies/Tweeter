e465acfec575ea0430ab70aa5f685550971e90a7
Max <jaimies.dev@gmail.com>
2020-10-30 19:54:11 +0200

    refactor(RxOperators.test.ts): use getValue() instead of expectObservableValue()

--- src/test/shared/RxOperators.test.ts
+++ src/test/shared/RxOperators.test.ts
@@ -1,15 +1,15 @@
 import {of} from "rxjs"
 import {filterList, mapList} from "@/shared/RxOperators"
-import {expectObservableValue} from "../RxTestUtils"
+import {getValue} from "../RxTestUtils"
 
-it("mapList()", () => {
+it("mapList()", async () => {
     const observable = of([1, 2])
     const mappedObservable = observable.pipe(mapList(item => item.toString()))
-    return expectObservableValue(mappedObservable, ["1", "2"])
+    expect(await getValue(mappedObservable)).toEqual(["1", "2"])
 })
 
-it("filterList()", () => {
+it("filterList()", async () => {
     const observable = of([1, 2])
     const mappedObservable = observable.pipe(filterList(item => item == 2))
-    return expectObservableValue(mappedObservable, [2])
+    expect(await getValue(mappedObservable)).toEqual([2])
 })
