77fd6af13187e71ca5847c3cb688c5109e5b365c
Max <jaimies.dev@gmail.com>
2020-11-03 09:39:34 +0200

    ui(UserPage): show spinner before the content is loaded

--- src/main/ui/pages/User.vue
+++ src/main/ui/pages/User.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="feed">
-    <UserCard @updateUser="user = $event" v-if="user" :user="user"/>
-    <ProfileNotFoundCard v-else/>
-
-    <TweetFeed :tweets="tweets"/>
+    <template v-if="!isLoading">
+      <UserCard @updateUser="user = $event" v-if="user" :user="user"/>
+      <ProfileNotFoundCard v-else/>
+      <TweetFeed :tweets="tweets"/>
+    </template>
+    <Spinner v-else/>
   </div>
 </template>
 
@@ -12,12 +14,13 @@ import {provideFindUserUseCase, provideGetTweetsByUserUseCase} from "@/di/provid
 import UserCard from "../components/user/Card"
 import ProfileNotFoundCard from "../components/user/NotFoundCard"
 import TweetFeed from "../components/tweet/Feed"
+import Spinner from "@/ui/components/ui/Spinner"
 
 const getTweets = provideGetTweetsByUserUseCase()
 const findUser = provideFindUserUseCase()
 
 export default {
-  components: {TweetFeed, ProfileNotFoundCard, UserCard},
+  components: {Spinner, TweetFeed, ProfileNotFoundCard, UserCard},
   props: {
     username: String
   },
@@ -25,11 +28,15 @@ export default {
     return {tweets: getTweets.run(this.username)}
   },
   data() {
-    return {user: null}
+    return {
+      user: null,
+      isLoading: true
+    }
   },
 
   async created() {
     this.user = await findUser.run(this.username)
+    this.isLoading = false
   }
 }
 </script>
