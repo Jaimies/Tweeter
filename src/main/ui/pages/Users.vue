3cb1e7e4c0487f4f41f753ca643f50db003ff1de
Max <jaimies.dev@gmail.com>
2020-11-03 10:43:56 +0200

    ui(UsersPage): show spinner before the content is loaded

--- src/main/ui/pages/Users.vue
+++ src/main/ui/pages/Users.vue
@@ -1,23 +1,27 @@
 <template>
-  <div class="feed">
+  <div class="feed" v-if="!isLoading">
     <UserListItem v-for="user of users" :key="user.id" :user="user"/>
   </div>
+  <Spinner v-else/>
 </template>
 
 <script>
 import {provideGetUsersUseCase} from "@/di/provideUseCases"
 import UserListItem from "../components/user/ListItem"
+import Spinner from "@/ui/components/ui/Spinner"
 
 export default {
-  components: {UserListItem},
+  components: {Spinner, UserListItem},
   data() {
     return {
+      isLoading: true,
       users: null
     }
   },
 
   async created() {
     this.users = await provideGetUsersUseCase().run()
+    this.isLoading = false
   }
 }
 </script>
