88f64a2e5abbef5db09d906cc31e0072fba34d3d
Max <jaimies.dev@gmail.com>
2020-11-04 22:07:01 +0200

    refactor(ListChange): make the constructor protected

--- src/main/domain/model/ListChange.ts
+++ src/main/domain/model/ListChange.ts
@@ -1,5 +1,5 @@
- export abstract class ListChange<T> {
-    constructor(public value: T) {}
+export abstract class ListChange<T> {
+    protected constructor(public value: T) {}
 
     static Add = class<T> extends ListChange<T> {
         constructor(value: T) { super(value) }
