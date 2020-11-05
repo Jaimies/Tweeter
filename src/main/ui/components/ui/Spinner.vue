b96530fb9d4f2a07ab81f2692251836327319bac
Max <jaimies.dev@gmail.com>
2020-11-05 10:07:45 +0200

    ui: show preloader before the scripts are downloaded

--- src/main/ui/components/ui/Spinner.vue
+++ src/main/ui/components/ui/Spinner.vue
@@ -5,30 +5,7 @@
 </template>
 
 <style lang="scss" scoped>
-$main-color: #80260f;
-$border-color: rgba($main-color, .2);
-
-.spinner {
-  width: 24px;
-  height: 24px;
-  border: 3px solid;
-  border-radius: 50%;
-  border-color: $border-color $border-color $border-color $main-color;
-  margin: auto;
-  animation: spin 1s linear infinite;
-}
-
 .wrapper {
   padding: 20px 10px;
-}
-
-@keyframes spin {
-  from {
-    transform: rotate(0deg);
-  }
-
-  to {
-    transform: rotate(360deg);
-  }
 }
 </style>
