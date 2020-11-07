3e193d1ecbba16e5407c9cd6016855e20db031c7
Max <jaimies.dev@gmail.com>
2020-11-07 13:32:51 +0200

    fix(UserListItem): don't show the follow button if the user is not authenticated

--- src/main/ui/components/user/ListItem.vue
+++ src/main/ui/components/user/ListItem.vue
@@ -4,18 +4,32 @@
     <div class="secondaryHeader">@{{ user.username }}</div>
     <p class="bio">{{ user.bio }}</p>
 
-    <FollowButton :user="user" @click.native.prevent/>
+    <FollowButton :user="user" v-if="isAuthenticated" @click.native.prevent/>
   </RouterLink>
 </template>
 
 <script>
 import {User} from "@/domain/model/User"
 import FollowButton from "@/ui/components/user/FollowButton"
+import {provideGetAuthStateUseCase} from "@/di/provideUseCases"
+import {AuthState} from "@/domain/model/AuthState"
+
+const getAuthState = provideGetAuthStateUseCase()
 
 export default {
   components: {FollowButton},
   props: {
     user: User
+  },
+
+  data() {
+    return {
+      isAuthenticated: false
+    }
+  },
+
+  async created() {
+    this.isAuthenticated = await getAuthState.run() == AuthState.Authenticated
   }
 }
 </script>
