051ed52e47c659c7c02fa8b40a7c5f05a5f96700
Max <jaimies.dev@gmail.com>
2020-11-10 22:07:27 +0200

    security: disallow posting tweets with someone else's name or username

--- firebase/rules.test.ts
+++ firebase/rules.test.ts
@@ -2,23 +2,29 @@ import * as firebase from "@firebase/rules-unit-testing"
 
 const appConfig = {projectId: "tweeter-dfa01", auth: {uid: "userId"}}
 const app = firebase.initializeTestApp(appConfig)
+const adminApp = firebase.initializeAdminApp(appConfig)
 const db = app.firestore()
+const adminDb = adminApp.firestore()
 
 afterEach(() => firebase.clearFirestoreData(appConfig))
-afterAll(() => db.terminate())
+afterAll(() => {
+    const terminateDb = db.terminate()
+    const terminateAdminDb = adminDb.terminate()
+    return Promise.all([terminateDb, terminateAdminDb])
+})
+
+const userData = {
+    username: "user",
+    name: "User",
+    email: "user@mail.com",
+    bio: "Bio",
+    following: [],
+}
 
 describe("users", () => {
     it("allows to read", () => {
         return firebase.assertSucceeds(db.doc("users/userId").get())
     })
-
-    const userData = {
-        username: "user",
-        name: "User",
-        email: "user@mail.com",
-        bio: "Bio",
-        following: [],
-    }
 
     it("allows user to create their profile", () => {
         const document = db.doc("users/userId")
@@ -40,20 +46,34 @@ describe("tweets", () => {
         body: "Tweet body",
         author: {
             id: "userId",
-            username: "username",
-            name: "Author's name",
+            username: userData.username,
+            name: userData.name,
         },
         date: firebase.firestore.FieldValue.serverTimestamp(),
     }
+
+    beforeEach(() => adminDb.doc("users/userId").set(userData))
 
     it("allows user to post on behalf of themselves", () => {
         const document = db.doc("tweets/tweetId")
         return firebase.assertSucceeds(document.set(tweetData))
     })
 
-    it("disallows posting on behalf of someone else", () => {
+    it("disallows posting on behalf of someone else's id", () => {
         const document = db.doc("tweets/tweetId")
         const tweetByOtherUser = {...tweetData, author: {...tweetData.author, id: "otherId"}}
+        return firebase.assertFails(document.set(tweetByOtherUser))
+    })
+
+    it("disallows posting with someone else's name", () => {
+        const document = db.doc("tweets/tweetId")
+        const tweetByOtherUser = {...tweetData, author: {...tweetData.author, name: "Other User"}}
+        return firebase.assertFails(document.set(tweetByOtherUser))
+    })
+
+    it("disallows posting with some else's username", () => {
+        const document = db.doc("tweets/tweetId")
+        const tweetByOtherUser = {...tweetData, author: {...tweetData.author, username: "otherUser"}}
         return firebase.assertFails(document.set(tweetByOtherUser))
     })
 
