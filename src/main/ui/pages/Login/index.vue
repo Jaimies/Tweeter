9537156accde99f0a52c569111e2a1d694c4352b
Max Poliakov <48586126+Jaimies@users.noreply.github.com>
2021-02-20 11:17:26 +0200

    ui(login): use input errors instead of separate error views

--- src/main/ui/pages/Login/index.vue
+++ src/main/ui/pages/Login/index.vue
@@ -3,10 +3,8 @@
     <template #heading>Login to Tweeter</template>
     <template #submitBtn>Login</template>
 
-    <LoginErrors :result="loginResult"/>
-
-    <BaseInput v-model="email" type="email" label="Email" :validation="$v.email" :error="emailError" />
-    <BaseInput v-model="password" label="Password" type="password" autocomplete="current-password"/>
+    <BaseInput v-model="email" type="email" label="Email" :validation="$v.email" :error="emailError"/>
+    <BaseInput v-model="password" label="Password" type="password" autocomplete="current-password" :validation="$v.password" :error="passwordError"/>
 
     <RouterLink tag="button" :to="{name: 'reset-password'}" class="forgot-your-password" type="button">
       Forgot your password?
@@ -19,13 +17,12 @@ import BaseInput from "../../components/ui/Input"
 import BaseForm from "../../components/ui/Form"
 import {provideLoginUseCase} from "@/di/provideUseCases"
 import {LoginResult} from "@/domain/repository/AuthRepository"
-import LoginErrors from "./Errors.vue"
 import {email, required} from "vuelidate/lib/validators"
 
 const login = provideLoginUseCase()
 
 export default {
-  components: {BaseForm, BaseInput, LoginErrors},
+  components: {BaseForm, BaseInput},
   data: () => ({
     email: "",
     password: "",
@@ -71,8 +68,13 @@ export default {
   },
   computed: {
     emailError() {
+      if (this.loginResult == LoginResult.UserNotFound) return "Couldn't find an account with that email address."
+      if(this.loginResult == LoginResult.TooManyAttempts) return "Too many failed login attempts. Please wait and try again."
       if (!this.$v.email.$error) return null
       if (!this.$v.email.email) return "Please enter a valid email"
+    },
+    passwordError() {
+      if (this.loginResult == LoginResult.WrongPassword) return "Wrong password. Try again or click Forgot password to reset it."
     },
   }
 }
