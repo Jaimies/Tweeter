a0dff1a88e3828bfb2559e277903ddb670e79711
Max Poliakov <48586126+Jaimies@users.noreply.github.com>
2021-03-08 18:38:23 +0200

    refactor: extract user list into a separate component

--- src/main/ui/pages/Users.vue
+++ src/main/ui/pages/Users.vue
@@ -1,30 +1,24 @@
 <template>
   <div class="container">
     <h1 class="v-heading mt-4 mb-6">Explore users</h1>
-    <div class="feed" v-if="!isLoading">
-      <UserListItem v-for="user of users" :key="user.id" :user="user"/>
-    </div>
-    <Spinner v-else/>
+    <UserList :users="users"/>
   </div>
 </template>
 
 <script>
 import {provideGetUsersUseCase} from "@/di/provideUseCases"
-import UserListItem from "../components/user/ListItem"
-import Spinner from "@/ui/components/ui/Spinner"
+import UserList from "@/ui/pages/UserList"
 
 export default {
-  components: {Spinner, UserListItem},
+  components: { UserList },
   data() {
     return {
-      isLoading: true,
       users: null
     }
   },
 
   async created() {
     this.users = await provideGetUsersUseCase().run()
-    this.isLoading = false
   }
 }
 </script>
