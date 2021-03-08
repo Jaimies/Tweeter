552c19febf88a7c335bb4aef4449e9a29e1af80e
Max Poliakov <48586126+Jaimies@users.noreply.github.com>
2021-03-08 19:04:41 +0200

    refactor(testData.createTestUser()): add parameters

--- src/test/testData.ts
+++ src/test/testData.ts
@@ -3,7 +3,6 @@ import {generateHash} from "./generateHash"
 
 export const testUser = createTestUser()
 
-export function createTestUser(): User {
-    const id = generateHash()
-    return new User(id, "username", "Test User", `${id}@gmail.com`, "User's bio", [], [])
+export function createTestUser(id = generateHash(), following: string[] = [], followers: string[] = []): User {
+    return new User(id, id, "Test User", `${id}@gmail.com`, "User's bio", following, followers)
 }
