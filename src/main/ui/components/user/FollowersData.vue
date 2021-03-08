c97f238e1b20040bffeedf281127e05f9e0ba03c
Max Poliakov <48586126+Jaimies@users.noreply.github.com>
2021-03-08 12:11:33 +0200

    feat: display the number of followers in profile

---
+++ src/main/ui/components/user/FollowersData.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="mt-3 mb-3">
+    <span class="label mr-5"><b>{{ user.followers.length }}</b> Followers</span>
+    <span class="label"><b>{{ user.following.length }}</b> Following</span>
+  </div>
+</template>
+
+<script>
+import {User} from "@/domain/model/User"
+
+export default {
+  props: {
+    user: User
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.label {
+  font-size: 15px;
+  color: #5b7083;
+
+  b {
+    color: #000;
+  }
+}
+</style>
